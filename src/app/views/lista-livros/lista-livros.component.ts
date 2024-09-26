8610a79b4f3d8413470ee9274105d505e4229b6c
DouglasIde <douglasymide@gmail.com>
2024-09-26 16:20:31 -0300

    Feat: Implementing the unsubscription of the search of the book

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,21 +7,25 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy{
 
   listaLivros: [];
   campoBusca = '';
+  subscription: Subscription;
 
   constructor(private service: LivroService) { }
 
   buscarLivros(){
-    this.service.buscar(this.campoBusca).subscribe({
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: returnAPI => console.log(returnAPI),
       error: erro => console.log(erro),
       complete: () => console.log('Obsevable completado')
     })
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
 
 
