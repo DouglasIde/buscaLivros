4dd9d30cad4b4d1657ebe11852d9793ded273546
DouglasIde <douglasymide@gmail.com>
2024-09-27 10:39:29 -0300

    Feat: Creating the flow after MAP in the service

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
-import { LivrosResultado } from './../models/interfaces';
+import { LivrosResultado, Item } from './../models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 
 @Injectable({
@@ -12,11 +12,13 @@ export class LivroService {
   private readonly API_Livro = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<LivrosResultado>{
+  buscar(valorDigitado: string): Observable<Item[]>{
     const params = new HttpParams().append('q', valorDigitado);
     return this.http.get<LivrosResultado>(this.API_Livro, { params })
     .pipe(
-      tap((returnAPI => console.log("Fluxo do TAP: ", returnAPI)))
+      tap((returnAPI => console.log("Fluxo do TAP: ", returnAPI))),
+      map(resultado => resultado.items),
+      tap(resultado => console.log("Fluxo após o MAP: ", resultado))
     );
   }
 }
